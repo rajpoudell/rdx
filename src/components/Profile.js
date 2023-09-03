1a82556f2528d4495c1ea0888d02915385df5d55
Raj Poudel <raj.poudeel@gmail.com>
2023-09-03 20:56:08 +0545

    profile and log in component

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Profile = () => {
+  return (
+    <div>
+        <button>Profile Pagee</button>
+        <p>Name:</p>
+        <p>Age:</p>
+        <p>Email:</p>
+    </div>
+  )
+}
