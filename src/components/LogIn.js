e1e9ea1366677b52a95e6e59366fcff1879507e6
Raj Poudel <raj.poudeel@gmail.com>
2023-09-05 12:18:34 +0545

    Login and profile

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,11 +1,17 @@
  import React from 'react'
+ import { useDispatch } from 'react-redux'
+ import {login} from "../features/user"
  
  const LogIn = () => {
+  const dispatch  = useDispatch();
    return (
      <div>
-        <button>Log In</button>
+        <button onClick={() => {
+          dispatch(login({name: "Raj", age:20, email: "rajpoudel@gmail.com"}));
+          
+          }}>Log In</button>
     </div>
    )
  }
- 
+  
  export default LogIn
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,7 @@ export  const Profile = () => {
     const user = useSelector((state) => state.user.value)
   return(
     <div>
-        <button>Profile Page</button>
+        <h1>Profile Page</h1>
         <p>Name:{user.name}</p>
         <p>Age:{user.age}</p>
         <p>Email: {user.email}</p>
